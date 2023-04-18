adae5783744425fd7fc392adec774e8b48ac1b74
kwak harin <ka28@naver.com>
2023-04-18 09:37:39 +0900

    axios 를 사용한 API 테스트 완료 및 hook 개발중...

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,1 +1,2 @@
-export const USER_SERVER = '/api/users';
+// export const USER_SERVER = '/api/users';
+export const USER_SERVER = '/gateway/restapi/v1.0';
--- frontend/src/views/APITest/APITestPage.js
+++ frontend/src/views/APITest/APITestPage.js
@@ -1,25 +1,124 @@
 // import{ SmileOutlined } from '@ant-design/icons';
 // import Item from 'antd/es/list/Item';
 import './APITest/page.css';
+import axiosApi from '../../utils/api/AxiosApi.js';
+import axios from 'axios';
 import React, {useState, useEffect} from "react";
+// import axios from 'axios';
+import { USER_SERVER } from '../../Config.js';
+
+const name = "ohsanrim";
+const phoneNum = "01000000000";
+const birth = "19999999";
+
+async function apiTestByGet(){
+        
+    const response = await axios.get(
+        USER_SERVER+"/APITestByGetLoading/"+name+"/"+phoneNum+"/"+birth
+      );
+      
+      return response.data;
+}
 
 function APITest() {
-    const [msg, setMsg] = useState("");
-    useEffect(() => {
-        fetch("/gateway/restapi/v1.0/queryDataByReactTest")
-            .then((res) => {return res.json();})
-            .then((data) => {setMsg(data);})
-      }, []);
+    
+
+    /*
+    Writer: Harin Kwak
+    Date: 2023/04/17
+    Description:API Test By Fetch (GET)
+    */
+    // const [getMsg, setGetMsg] = useState("");  //Get
+    // useEffect(() => {
+    //     fetch("/gateway/restapi/v1.0/APITestByGet/"+name+"/"+phoneNum+"/"+birth, { method: "GET" })
+    //         .then((res) => {return res.json();})
+    //         .then((data) => {setGetMsg(data);})
+    //   }, []);
+
+    /*
+    Writer: Harin Kwak
+    Date: 2023/04/17
+    Description:API Test By Axios (GET)
+    */
+    // useEffect(() => { 
+    //     axios.get("/gateway/restapi/v1.0/APITestByGet/"+name+"/"+phoneNum+"/"+birth)
+    //   .then((res)=>{
+    //     setGetMsg(res.data);})
+    //   .catch((err)=> {console.log(err);})
+    // }, []);
+
+    /*
+    Writer: Harin Kwak
+    Date: 2023/04/17
+    Description:API Test By Fetch (POST)
+    */
+    // const [postMsg, setPostMsg] = useState("");  //POST
+    // useEffect(() => {
+    //     fetch("/gateway/restapi/v1.0/APITestByPost", { 
+    //         method: "POST",
+    //         headers : {               //데이터 타입 지정
+    //             "Content-Type":"application/json; charset=utf-8",
+    //             "Name": name,
+    //             "Birth": birth, 
+    //             "PhoneNum": phoneNum
+    //         }, 
+    //         // body: JSON.stringify(jsonData)
+    //         body: jsonData
+    //     })
+    //         .then((res) => {
+    //             console.log(res.json());
+    //             return res.json();
+    //         })
+    //         .then((data) => {setPostMsg(data);})
+    //   }, []);
+
+    /*
+    Writer: Harin Kwak
+    Date: 2023/04/17
+    Description:API Test By Axios (POST)
+    */
+    // useEffect(() => { 
+    //     axios.post("/gateway/restapi/v1.0/APITestByPost", jsonData, { 
+    //         headers:{ 
+    //             "Content-Type":"application/json; charset=utf-8",
+    //             "Name": name,
+    //             "Birth": birth, 
+    //             "PhoneNum": phoneNum
+    //            } 
+    //          })
+    //   .then((res)=>{
+    //     setPostMsg(res.data);
+    // })
+    //   .catch((err)=> {console.log(err);})
+    // }, []);
+
+
+    const [state, refetch] = axiosApi(apiTestByGet, []);
+    const { loading, data: datas, error } = state;
+
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>에러가 발생했습니다</div>;
+    if (!datas) return null;
+    
     return (
         <div className="container">
-            <div class="insertTestWrapper">
+            <div className="insertTestWrapper">
 
             </div>
-            <div class="queryTestWrapper">
+            <div className="queryTestWrapper">
+                <h3>GET 방식</h3>
                 <ul>
-                    <li><label>이름: </label>{msg.name}</li>
-                    <li><label>생일: </label>{msg.birth}</li>
-                    <li><label>핸드폰번호: </label>{msg.phoneNum}</li>
+                    <li><label>이름: </label>{datas.name}</li>
+                    <li><label>생일: </label>{datas.birth}</li>
+                    <li><label>핸드폰번호: </label>{datas.phoneNum}</li>
+                </ul>
+            </div>
+            <div className="queryTestWrapperByPost">
+                <h3>POST 방식</h3>
+                <ul>
+                    <li><label>이름: </label>{datas.name}</li>
+                    <li><label>생일: </label>{datas.birth}</li>
+                    <li><label>핸드폰번호: </label>{datas.phoneNum}</li>
                 </ul>
             </div>
         </div>
